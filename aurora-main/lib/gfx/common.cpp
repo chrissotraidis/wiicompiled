cbbf9eb00b7ef1ebecf51ea8381031cedeb96545
Chris Sotraidis <56282582+chrissotraidis@users.noreply.github.com>
2026-09-19 21:48:14 +0900

    Observe device loss while waiting for a staging map

--- aurora-main/lib/gfx/common.cpp
+++ aurora-main/lib/gfx/common.cpp
@@ -1012,6 +1012,7 @@ static bool begin_frame_impl(bool clearEfb) {
         return false;
       }
       g_instance.ProcessEvents();
+      webgpu::fail_if_device_lost();
       s_mappingState.wait_for_progress();
     }
   }
